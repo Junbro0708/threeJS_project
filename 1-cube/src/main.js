93046e65cbb2d4eb18d9a4ea8332e52201812da7
Junbro0708 <aktm708@naver.com>
2023-10-01 20:57:21 +0900

    Update main.js

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -8,8 +8,9 @@ function init() {
   // Added renderer
   const renderer = new THREE.WebGLRenderer({
     // alpha: true
+    antialias: true
   })
-  
+
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 
@@ -23,6 +24,27 @@ function init() {
     1, // near
     500 // far
   )
+  // camera.position.z = 5;
+  camera.position.set(3, 4, 5)
+  
+  // Added Mesh
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xcc99ff
+  })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube);
+  
+  camera.lookAt(cube.position)
+
+  // Added light
+  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1)
+  directionalLight.position.set(-1, 2, 3)
+  scene.add(directionalLight)
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+  ambientLight.position.set(3, 2, 1)
+  scene.add(ambientLight)
 
   renderer.render(scene, camera)
 }
