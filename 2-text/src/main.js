3920b177e3b44c5451caade27f40f7ea1c8b9f0f
Junbro0708 <aktm708@naver.com>
2023-10-03 19:08:52 +0900

    added new project

---
+++ 2-text/src/main.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three'
+
+window.addEventListener('load', function() {
+  init()
+})
+
+function init() {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true
+  })
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  // Added scene
+  const scene = new THREE.Scene()
+
+  //Added Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  )
+  camera.position.z = 5;
+
+  function render() {
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(render)
+  }
+
+  render()
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+  }
+
+  window.addEventListener("resize", handleResize)
+}
