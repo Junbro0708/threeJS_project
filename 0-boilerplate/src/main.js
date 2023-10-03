274d76121fc50a0ddf9073cbc5a35791a6f6523e
Junbro0708 <aktm708@naver.com>
2023-10-03 16:36:57 +0900

    modified boiler plate code

--- 0-boilerplate/src/main.js
+++ 0-boilerplate/src/main.js
@@ -5,5 +5,40 @@ window.addEventListener('load', function() {
 })
 
 function init() {
-  console.log(THREE)
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true
+  })
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  // Added scene
+  const scene = new THREE.Scene()
+
+  //Added Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  )
+  camera.position.z = 5;
+
+  function render() {
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(render)
+  }
+
+  render()
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+  }
+
+  window.addEventListener("resize", handleResize)
 }
